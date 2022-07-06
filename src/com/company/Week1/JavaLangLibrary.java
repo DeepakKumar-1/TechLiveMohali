c9de9339d590ed368ff1f9a63de49f3149ce067e
Deepak <dk8400779@gmail.com>
2022-07-06 10:18:51 +0530

    Initial commit

---
+++ src/com/company/Week1/JavaLangLibrary.java
@@ -0,0 +1,22 @@
+package com.company.Week1;
+import java.lang.*;
+public class JavaLangLibrary {
+    public static void main(String[] args) {
+        System.out.println(Math.max(12, 34));
+        System.out.println(Math.min(2,3));
+        System.out.println(Math.abs(-4656));
+        System.out.println(Math.sqrt(246));
+        System.out.println(Math.cbrt(246)); // cube Root
+        System.out.println(Math.ceil(4.56));
+        System.out.println(Math.pow(2, 4));
+        System.out.println(Math.random());
+        System.out.println(Math.addExact(23, 45));
+        System.out.println(Math.subtractExact(44, 5));
+        System.out.println(Math.multiplyExact(44, 5));
+        System.out.println(Math.floorDiv(55, 5));
+        System.out.println(Math.asin(45));
+        System.out.println(Math.sin(90));
+        System.out.println(Math.tan(90));
+        System.out.println(Math.atan(90));
+    }
+}
