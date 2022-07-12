8b91974fde17e7c3435d1836e0889316d75d6d66
Deepak <dk8400779@gmail.com>
2022-07-12 15:19:57 +0530

    Initial Commit

---
+++ src/com/company/AWT/FirstApp.java
@@ -0,0 +1,8 @@
+package com.company.AWT;
+import java.awt.*;
+public class FirstApp {
+    public static void main(String []args){
+        Frame f = new Frame("My First App");
+        f.setVisible(true);
+    }
+}
