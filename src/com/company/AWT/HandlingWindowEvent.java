4d3de1a0e6468d03af8b625f27491ace99aae775
Deepak <dk8400779@gmail.com>
2022-07-14 16:26:13 +0530

    Initial Commit

--- src/com/company/AWT/HandlingWindowEvent.java
+++ src/com/company/AWT/HandlingWindowEvent.java
@@ -9,6 +9,43 @@ class MyFrame16 extends Frame implements  WindowListener{
         setLayout(new FlowLayout());
         l = new Label("");
         add(l);
+        addWindowListener(this);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        l.setText("Window Opened");
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        l.setText("Window Closing");
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        l.setText("Window Closed");
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        l.setText("Window Iconified");
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        l.setText("Window Deiconified");
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        l.setText("Window Activated");
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        l.setText("Window Deactivated");
     }
 }
 
