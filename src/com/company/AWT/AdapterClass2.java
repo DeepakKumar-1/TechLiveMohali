d58bb8e563c05aac3cc06ceab7ab19ac8be548d2
Deepak <dk8400779@gmail.com>
2022-07-14 23:00:02 +0530

    Initial Commit

--- src/com/company/AWT/AdapterClass2.java
+++ src/com/company/AWT/AdapterClass2.java
@@ -1,23 +1,23 @@
-package com.company.AWT;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-class MyFrame18 extends Frame{
-    MyFrame18(){
-        super("Window Adapter");
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-    }
-}
-public class AdapterClass2 {
-    public static void main(String []args){
-        MyFrame18 f = new MyFrame18();
-        f.setSize(500, 500);
-        f.setVisible(true);
-    }
-}
+//package com.company.AWT;
+//import java.awt.*;
+//import java.awt.event.*;
+//
+//class MyFrame18 extends Frame{
+//
+//    MyFrame18(){
+//        super("Window Adapter");
+//        addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//                System.exit(0);
+//            }
+//        });
+//    }
+//}
+//public class AdapterClass2 {
+//    public static void main(String []args){
+//        MyFrame18 f = new MyFrame18();
+//        f.setSize(500, 500);
+//        f.setVisible(true);
+//    }
+//}
---
+++ src/com/company/AWT/DifferentWaysOfHandlingEvents.java
@@ -0,0 +1,110 @@
+package com.company.AWT;
+import java.awt.*;
+import java.awt.event.*;
+
+// Method 1 : Frame Class
+//class MyFrame18 extends Frame implements ActionListener{
+//    TextField tf;
+//    Button b;
+//    int count = 0;
+//    MyFrame18(){
+//        super("Event Demo");
+//        tf = new TextField(count+"",20);
+//        b  = new Button("Click");
+//
+//        setLayout(new FlowLayout());
+//
+//        add(tf);
+//        add(b);
+//        b.addActionListener(this);
+//    }
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        count++;
+//        tf.setText(""+count);
+//    }
+//}
+
+
+// Method 2 : Inner Class
+//class MyFrame18 extends Frame{
+//    TextField tf;
+//    Button b;
+//    int count = 0;
+//    MyFrame18(){
+//        super("Event Demo");
+//        tf = new TextField(count+"",20);
+//        b  = new Button("Click");
+//
+//        setLayout(new FlowLayout());
+//
+//        add(tf);
+//        add(b);
+//        b.addActionListener(new MyActionListener());
+//    }
+//
+//     class MyActionListener implements ActionListener {
+//         public void actionPerformed(ActionEvent e) {
+//             count++;
+//             tf.setText("" + count);
+//         }
+//     }
+//}
+
+
+// Method 3 : Anonymous Class
+//class MyFrame18 extends Frame{
+//    TextField tf;
+//    Button b;
+//    int count = 0;
+//    MyFrame18(){
+//        super("Event Demo");
+//        tf = new TextField(count+"",20);
+//        b  = new Button("Click");
+//
+//        setLayout(new FlowLayout());
+//
+//        add(tf);
+//        add(b);
+//        b.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                count++;
+//                tf.setText("" + count);
+//            }
+//        });
+//    }
+//}
+
+// Method 4 : Lambda Expression
+
+class MyFrame18 extends Frame{
+    TextField tf;
+    Button b;
+    int count = 0;
+    MyFrame18(){
+        super("Event Demo");
+        tf = new TextField(count+"",20);
+        b  = new Button("Click");
+
+        setLayout(new FlowLayout());
+
+        add(tf);
+        add(b);
+        b.addActionListener((ActionEvent ae) ->  {
+            count++;
+            tf.setText("" + count);
+        });
+    }
+
+
+}
+
+public class DifferentWaysOfHandlingEvents {
+    public static void main(String []args){
+        MyFrame18 f = new MyFrame18();
+        f.setSize(500, 500);
+        f.setVisible(true);
+    }
+}
